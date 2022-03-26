a213efc1380c04006bdf646de7b44395d850de0e
Giulio899 <giulioposati17@hotmail.it>
2022-03-26 12:25:12 +0100

    Added game activity

--- ChessReferee/app/src/main/java/com/example/chessrefereeapp/MainActivity.kt
+++ ChessReferee/app/src/main/java/com/example/chessrefereeapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.chessrefereeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startGame(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 }
